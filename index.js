908eaf6c3a323947fb995c4bcb6a5db4894daa7b
bsorinnn <bsorinn@example.com>
2021-02-15 11:07:48 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 const express = require("express"); //express 모듈을 가져온다.
 const app = express(); // 모듈의 펑션을 이용해서 새로운 express APP을 생성한다
 const port = 5000;
+const bodyParser = require("body-parser");
+const config = require("./config/key");
+const { User } = require("./models/Users");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://sohhho:clust3r1526@cluster0.x2eua.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("MongoDB Connected...")) //질 연결되었는지 확인
   .catch((err) => console.log(err)); // 에러 발생하면 알려줌
 
-app.get("/", (req, res) => res.send("Hello World! Welcome to Node JS World!"));
+app.get("/", (req, res) => res.send("JS가 좋니"));
+
+app.post("/register", (req, res) => {
+  //회원 가입 시 필요한 정보들을 클라이언트에서 가져오면
+  //그것들을 데이터베이스에 넣어준다.
+
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`)); //5000번 포트에서 앱 실행
